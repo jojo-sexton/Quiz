0bbe55c5946bee1a16dedeaada8cc61e7c4b02c3
jojo sexton <jojolsexton@gmail.com>
2023-08-12 11:33:05 +1200

    the end of unfortunate souls

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,14 @@
+const pathetics = ["The miserable", "The lonely", "The depressed" ]
 
+const ul = document.querySelector('.content > ul')
+
+// // producing html li by arraymap
+// const pathHTML = pathetics.map(p => `<li>${p}</li>`)
+// // inject li to ul
+// ul.innerHTML = pathHTML.join("")
+
+
+// create and append li to html
+pathetics.forEach (p => {
+  ul.innerHTML += `<li>${p}</li>`
+})
