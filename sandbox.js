a116036331c322ae325dcac0262f86457485d73e
jojo sexton <jojolsexton@gmail.com>
2023-08-12 23:16:26 +1200

    add remove and toggle a class usising elemement.classList.add/remove/toggle

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,23 @@
-const pathetics = ["The miserable", "The lonely", "The depressed" ]
+// Task: if the paragraph has the word success, assign the class success to the paragraph. If error, assign class error. 
 
-const ul = document.querySelector('.content > ul')
-
-// // producing html li by arraymap
-// const pathHTML = pathetics.map(p => `<li>${p}</li>`)
-// // inject li to ul
-// ul.innerHTML = pathHTML.join("")
+// first of all get the reference to all the p tags, this will return a node list so that we can cycle thru them
+const paras = document.querySelectorAll ('.content > p')
 
 
-// create and append li to html
-pathetics.forEach (p => {
-  ul.innerHTML += `<li>${p}</li>`
+// cycle thru the node list and check if the para has the word error or success in it
+paras.forEach(p => {
+  if (p.innerHTML.includes('success')){
+    p.classList.add('success')
+  } 
+  else if (p.innerHTML.includes('error')){
+    p.classList.add('eror')
+  }
+
 })
 
+paras.forEach(p => console.log(p.classList))
+
+
+// Toggle a class, if h1 has the class title, this code will remove it and vice versa
+const title = document.querySelector('h1')
+title.classList.toggle('title')
