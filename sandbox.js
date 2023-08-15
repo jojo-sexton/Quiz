63df7f1cb411b6288c26c6c418efffda15e95353
jojo sexton <jojolsexton@gmail.com>
2023-08-16 10:57:37 +1200

    basic dom events, event bubbling, stopPropagation to stop event bubbling

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,58 @@
-const pathetics = ["The miserable", "The lonely", "The depressed" ]
+// // when click on a task, it strike off that task
+// const tasks = document.querySelectorAll('li')
 
-const ul = document.querySelector('.content > ul')
+// tasks.forEach(t => {
+//   t.addEventListener ('click', e => {
+//     e.target.style.textDecoration = 'line-through'
 
-// // producing html li by arraymap
-// const pathHTML = pathetics.map(p => `<li>${p}</li>`)
-// // inject li to ul
-// ul.innerHTML = pathHTML.join("")
+//   })
+// })
 
 
-// create and append li to html
-pathetics.forEach (p => {
-  ul.innerHTML += `<li>${p}</li>`
+// when click on "add new todo" button, "something new" is added to the top of the list
+
+// grab the botton
+
+const ul = document.querySelector('ul')
+
+const button = document.querySelector('button')
+button.addEventListener('click', e => {
+const newLi = document.createElement ('li')
+newLi.textContent = 'something new'
+ul.prepend(newLi)
 })
 
+// // click on the todo to delete it (this does not work on "something new")
+// const todos = document.querySelectorAll ('li')
+// todos.forEach(todo => {
+//   todo.addEventListener ('click', e => {
+//     e.target.remove()
+//   })
+// })
+
+// Click on todo to delete it (works on "something new")
+ul.addEventListener('click', e => {
+  console.log('event in ul')
+  if(e.target.nodeName === "LI") {
+    e.target.remove()
+  }
+  e.stopPropagation()
+
+})
+
+
+// e.stopPropagation to stop the bubbling of execution of click event bubbling up from ul to its parents content
+const content = document.querySelector('.content')
+content.addEventListener('click', e => console.log ('event in content'))
+
+// more DoM event
+//  COPY EVENT: do something when user try to copy the text "Do not copy me"
+const notCopy = document.querySelector ('.copy')
+notCopy.addEventListener ('copy', () => {console.log('OI! i am copyright')})
+
+// MOUSE MOVE EVENT
+const box = document.querySelector ('.box')
+box.addEventListener('mousemove', e => {
+  box.textContent = `X pos is ${e.offsetX} pixels from the left and Y position is ${e.offsetY} pixels from the top`
+ 
+})
