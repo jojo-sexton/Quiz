345fab0a839666fcc8bc93506fbad5b9f4b5b97d
jojo sexton <jojolsexton@gmail.com>
2023-08-18 08:32:57 +1200

    form validation

--- sandbox.js
+++ sandbox.js
@@ -1,37 +1,52 @@
 const form = document.querySelector('form')
+const feedback = document.querySelector('.feedback')
 
-// console log the username that is input by user
+
+// console log the username that is input by user, the user name must contain atleast 6 characters, lower case with no special character
 form.addEventListener('submit', e =>{
   e.preventDefault()
-  console.log(form.userName.value)
+
+
+  // validation:
+  const username = form.userName.value
+  const pattern = /^[a-z]{6,}$/
+  const result = pattern.test(username)
+
+  
+  if (result){
+    feedback.textContent='That username is valid'
+  } else {
+    feedback.textContent='The user name must be at least 6 characters long, lower case with no special character '
+  }
 })
 
-// Testing the RegEx:  at least 6 letters, lower case, no special character
 
-const username = 'poppysexton'
+// // Testing the RegEx:  at least 6 letters, lower case, no special character
 
-// create a patern
-const pattern = /^[a-z]{6,}$/
+// const username = 'poppysexton'
 
-// METHOD 1 USING PATERN.TEST() to test if the username matches the patern requirement. this function will return a boolean value 
-const result = pattern.test(username)
+// // create a patern
+// const pattern = /^[a-z]{6,}$/
 
-if (result){
-  console.log('RegEx test passed :)')
-} else {
-  console.log('RegEx test failed :(')
-}
+// // METHOD 1 USING PATERN.TEST() to test if the username matches the patern requirement. this function will return a boolean value 
+// const result = pattern.test(username)
 
-// METHOD 2: SEARCH THE REGEX PATTERN IN THE STRING USERNAME, IF IT DOESNT MATCH, IT WILL RETURN -1, IF IT DOES IT WILL RETURN 0, 1, 2, OR 3 DEPENDS ON WHERE THE PATERN STARTS IN THE STRING
-const username1 = 'pricesspeach$9445'
-const pattern1 = /^[a-z]{6,}$/
-const result1 = username1.search(pattern1)
-console.log(result1)
-if (result1<0){
-  console.log('Regex test failed :(')}
-  else {
-    console.log('Regex test passed')
-  }
+// if (result){
+//   console.log('RegEx test passed :)')
+// } else {
+//   console.log('RegEx test failed :(')
+// }
+
+// // METHOD 2: SEARCH THE REGEX PATTERN IN THE STRING USERNAME, IF IT DOESNT MATCH, IT WILL RETURN -1, IF IT DOES IT WILL RETURN 0, 1, 2, OR 3 DEPENDS ON WHERE THE PATERN STARTS IN THE STRING
+// const username1 = 'pricesspeach$9445'
+// const pattern1 = /^[a-z]{6,}$/
+// const result1 = username1.search(pattern1)
+// console.log(result1)
+// if (result1<0){
+//   console.log('Regex test failed :(')}
+//   else {
+//     console.log('Regex test passed')
+//   }
 
 
 
